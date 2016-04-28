9d942c68035311efbe5541b0d4fb17cfe8e03d12
TFavrot <thomas.favrot@insa-lyon.fr>
2016-04-28 10:06:36 +0200

    Trip display added

---
+++ app/src/main/java/com/pld/velociraptor/service/TripService.java
@@ -0,0 +1,15 @@
+package com.pld.velociraptor.service;
+
+import javax.inject.Singleton;
+
+/**
+ * Created by thomas on 28/04/2016.
+ */
+@Singleton
+public class TripService {
+
+    public void loadTrips(TripLoadedCallback tripLoadedCallback){
+
+    }
+
+}
